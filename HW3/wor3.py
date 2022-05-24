4dfae262037d4c4440159c0bac31ec08ea47e28e
orlivne <livneor207@gmail.com>
2022-05-24 14:21:50 +0300

    ...

--- HW3/wor3.py
+++ HW3/wor3.py
@@ -7,6 +7,7 @@ import numpy as np
 from random import shuffle
 from sklearn.metrics import classification_report
 from tqdm import tqdm
+import pandas as pd
 ############################################################################################################################
 main_folder = r'C:\MSC\NLP2\HW3'
 train_path = os.path.join(main_folder, 'connl03_train.txt')
@@ -178,10 +179,53 @@ class NERNet(nn.Module):
 write a training loop, which takes a model (instance of NERNet) and number of epochs to train on.
 The loss is always CrossEntropyLoss and the optimizer is always Adam.
 """
+def get_model_results(model, test_sequences):
+    """
+    
+
+    Parameters
+    ----------
+    model : Torch model  - 
+        DESCRIPTION: LSTM model.
+    test_sequences : list
+        DESCRIPTION: input list of coupels [[word_tensor, lebel_tensor] , ...]
+    
+    the function get model results
+    
+    Returns
+    -------
+    all_test_words_pred : list
+    all_test_words_true : list
+    binary_test_words_pred : list
+    binary_test_words_true : list
+    """
+    # generate test tokens prediction
+    all_test_words_pred = []
+    all_test_words_true = []
+
+    # generate test binnary prediction
+    binary_test_words_pred = []
+    binary_test_words_true = []
+    for sentence, labels in test_sequences:
+        sentence_tensor = torch.LongTensor(sentence).cuda()
+        labels_tensor = torch.LongTensor(labels).cuda()
+
+        _, pred_labels = model(sentence_tensor).T.max(0)
+
+        all_test_words_pred += pred_labels.tolist()
+        all_test_words_true += labels.tolist()
+
+        binary_test_words_pred += [1 if i >= 1 else i for i in all_test_words_pred]
+        binary_test_words_true += [1 if i >= 1 else i for i in all_test_words_true]
+    return all_test_words_pred, all_test_words_true, binary_test_words_pred, binary_test_words_true
 
 
 
-def train_loop(model, n_epochs):
+def train_loop(model, n_epochs, train_sequences):
+    #
+    all_target_names = ["O", "B-PER", "I-PER", "B-LOC", "I-LOC", "B-ORG", "I-ORG"]
+    binary_target_names = ["O", "OTHERS"]
+    
     # Loss function
     criterion = nn.CrossEntropyLoss()
 
@@ -193,7 +237,7 @@ def train_loop(model, n_epochs):
     STEP = 400 
     
     for e in range(1, n_epochs + 1):
-        print('ephoc 1')
+        print('start ephoc #' + str(e))
         # TODO - your code goes here...
         """
         tqdm - add progress bar
@@ -223,7 +267,14 @@ def train_loop(model, n_epochs):
             
             # once the gradients are computed use them to optimize model
             optimizer.step()
-            
+            print('start ephoc #' + str(e))
+        
+        print('finshed ephoc #' + str(e) + ', ephoch results:' )
+        all_train_words_pred, all_train_words_true, \
+        binary_train_words_pred, binary_train_words_true = get_model_results(model, train_sequences)
+        train_Results_dict = pd.DataFrame(classification_report(all_train_words_true, all_train_words_pred, target_names=all_target_names))
+        print(train_Results_dict)
+
             
             
 """
@@ -235,60 +286,130 @@ The caption argument for the function should be served for printing, so that whe
 """
 
 
-def evaluate(model, caption):
+def evaluate(model, caption, test_sequences, dev_sequences):
     # TODO - your code goes here
     # from Piazza: https://piazza.com/class/klxc3m1tzqz2o8?cid=59
 
     all_target_names = ["O", "B-PER", "I-PER", "B-LOC", "I-LOC", "B-ORG", "I-ORG"]
     binary_target_names = ["O", "OTHERS"]
-
+    
+    # self.tag2id = {"O":0, "B-PER":1, "I-PER": 2, "B-LOC": 3, "I-LOC": 4, "B-ORG": 5, "I-ORG": 6}
+    # self.id2tag = {0:"O", 1:"B-PER", 2:"I-PER", 3:"B-LOC", 4:"I-LOC", 5:"B-ORG", 6:"I-ORG"}
+    
     print(f"****************    Results for {caption}    ****************")
 
-    # evaluate test
-    all_test_words_pred = []
-    all_test_words_true = []
-    binary_test_words_pred = []
-    binary_test_words_true = []
 
+
+    # generate dev tokens prediction 
     all_dev_words_pred = []
     all_dev_words_true = []
+    
+    # generate dev binnary prediction 
     binary_dev_words_pred = []
     binary_dev_words_true = []
 
-    for sentence, labels in test_sequences:
-        sentence_tensor = torch.LongTensor(sentence).cuda()
-        labels_tensor = torch.LongTensor(labels).cuda()
-        
-        _, pred_labels = model(sentence_tensor).T.max(0)
+    # get test results
+    all_test_words_pred, all_test_words_true, \
+        binary_test_words_pred, binary_test_words_true = get_model_results(model, test_sequences)
 
-        all_test_words_pred += pred_labels.tolist()
-        all_test_words_true += labels.tolist()
-        
-        binary_test_words_pred += [1 if i >=1 else i for i in all_test_words_pred]
-        binary_test_words_true += [1 if i >=1 else i for i in all_test_words_true]
+    # get dev results
+    all_dev_words_pred, all_dev_words_true, \
+        binary_dev_words_pred, binary_dev_words_true = get_model_results(model, dev_sequences)
 
-    # evaluate dev
-    for sentence, labels in dev_sequences:
-        sentence_tensor = torch.LongTensor(sentence).cuda()
-        labels_tensor = torch.LongTensor(labels).cuda()
-        
-        _, pred_labels = model(sentence_tensor).T.max(0)
+    print("Test Results:")
+    Test_Results_dict = pd.DataFrame(classification_report(all_test_words_true, all_test_words_pred, target_names=all_target_names))
+    print(Test_Results_dict)
+    print("Dev Results:")
+    Dev_Results_dict = pd.DataFrame(classification_report(all_dev_words_true, all_dev_words_pred, target_names=all_target_names))
+    print(Dev_Results_dict)
 
-        all_dev_words_pred += pred_labels.tolist()
-        all_dev_words_true += labels.tolist()
+    print("Binary Test Results:")
+    Binary_Test_Results = pd.DataFrame(classification_report(binary_test_words_true, binary_test_words_pred, target_names=binary_target_names))
+    print(Binary_Test_Results)
+
+    print("Binary Dev Results:")
+    Binary_Dev_Results  = pd.DataFrame(classification_report(binary_dev_words_true, binary_dev_words_pred, target_names=binary_target_names))
+    print(Binary_Dev_Results)
+
+    return 
+
+
+
+"""
+**Task 6:**
+
+Train and evaluate a few models, all with embedding_size=300, 
+and with the following hyper parameters (you may use that as captions for the models as well):
+
+Model 1: (hidden_size: 500, n_layers: 1, directions: 1)
+
+Model 2: (hidden_size: 500, n_layers: 2, directions: 1)
+
+Model 3: (hidden_size: 500, n_layers: 3, directions: 1)
+
+Model 4: (hidden_size: 500, n_layers: 1, directions: 2)
+
+Model 5: (hidden_size: 500, n_layers: 2, directions: 2)
+
+Model 6: (hidden_size: 500, n_layers: 3, directions: 2)
+
+Model 7: (hidden_size: 800, n_layers: 1, directions: 2)
+
+Model 8: (hidden_size: 800, n_layers: 2, directions: 2)
+
+Model 9: (hidden_size: 800, n_layers: 3, directions: 2)
+
+"""
+
+
+# TODO - your code goes here...
+EMBEDDING_SIZE = 300
+EPOCHS = 10
+HIDDEN_SIZE  = 500 
+INPUT_SIZE = len(vocab.word2id) # 8955
+OUTPUT_SIZE = len(vocab.tag2id) # 7
+
+
+n_layers_array = np.arange(1,4)
+directions_array = np.arange(1,3)
+model_list  = []
+for i_n_layers in n_layers_array:
+    for i_directions in directions_array:
+        print('Train model using:\n'+ \
+              '1)hidden_size = ' + str(HIDDEN_SIZE)+'\n'+ \
+              '2)n_layers = ' + str(i_n_layers) + '\n'+ \
+              '3)directions = ' + str(i_directions))
+        model = NERNet(INPUT_SIZE, EMBEDDING_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, i_n_layers, i_directions).cuda()
+        train_loop(model, EPOCHS)
+        model_list.append(model)
+
+DIRECTION = 2
+HIDDEN_SIZE = 800
+for i_n_layers in n_layers_array:
+        print('Train model using:\n'+ \
+              '1)hidden_size = ' + str(HIDDEN_SIZE)+'\n'+ \
+              '2)n_layers = ' + str(i_n_layers) + '\n'+ \
+              '3)directions = ' + str(i_directions))
+        model = NERNet(INPUT_SIZE, EMBEDDING_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, i_n_layers, DIRECTION).cuda()
+        train_loop(model, EPOCHS)
+        model_list.append(model)
+        
+  
     
-        binary_dev_words_pred += [1 if i >=1 else i for i in all_dev_words_pred]
-        binary_dev_words_true += [1 if i >=1 else i for i in all_dev_words_true]
+for i, model in enumerate(model_list):
+    model_name = "model_"+str(i)
+    evaluate(model, model_name, test_sequences, dev_sequences)
+    
+    
+        
+"""
+**Task 6:** 
+Download the GloVe embeddings from https://nlp.stanford.edu/projects/glove/ (use the 300-dim vectors from glove.6B.zip). 
+Then intialize the nn.Embedding module in your NERNet with these embeddings, 
+so that you can start your training with pre-trained vectors.
+Repeat Task 6 and print the results for each model.
+Note: make sure that vectors are aligned with the IDs in your Vocab, in other words, make sure that for example the word with ID 0 is the first vector in the GloVe matrix of vectors that you initialize nn.Embedding with. For a dicussion on how to do that, check it this link:
+https://discuss.pytorch.org/t/can-we-use-pre-trained-word-embeddings-for-weight-initialization-in-nn-embedding/1222
+"""
 
-    print("ALL Test Results:")
-    print(classification_report(all_test_words_true, all_test_words_pred, target_names=all_target_names))
-
-    print("ALL Dev Results:")
-    print(classification_report(all_dev_words_true, all_dev_words_pred, target_names=all_target_names))
-
-    print("BINARY Test Results:")
-    print(classification_report(binary_test_words_true, binary_test_words_pred, target_names=binary_target_names))
-
-    print("BINARY Dev Results:")
-    print(classification_report(binary_dev_words_true, binary_dev_words_pred, target_names=binary_target_names))
 
